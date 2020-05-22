473a2ed1166634d1707168ad392d78191af08d84
peterennis <peterennis@yahoo.com>
2020-05-21 21:17:56 -0700

    chore(): trivial page change to test refresh update

--- src/components/nonCmsPage/index.js
+++ src/components/nonCmsPage/index.js
@@ -5,7 +5,7 @@ const NonCmsPage = props => (
     <div className="container">
       <div className="row">
         <div className="col-12">
-          <h1>This page is hardcoded</h1>
+          <h1>This page is REALLY hardcoded</h1>
           <p>
             That's the beauty, cms driven does not restrict you. You can use the
             cms side by side with your custom pages.
