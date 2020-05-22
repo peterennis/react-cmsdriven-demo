37264d63f081693156b2e0c96a22979ea3019adf
peterennis <peterennis@yahoo.com>
2020-05-22 09:46:04 -0700

    Update index.stories.js

--- src/components/Hero/index.stories.js
+++ src/components/Hero/index.stories.js
@@ -18,10 +18,10 @@ export default {
 
 export const BasicHero = () => {
   return (
-   <Hero
-   title={text("title","Elegant e-shop")}
-   paragraph={text("paragraph","<h3>This is a shop made in react, storybook and cmsdriven</h3>")}
-   firstCTA={text("firstCTA","Book a call")}
-   secondCTA={text("secondCTA","Learn more")}></Hero>
+    <Hero
+      title={text("title", "Elegant e-shop")}
+      paragraph={text("paragraph", "<h3>This is a shop made in react, storybook and cmsdriven</h3>")}
+      firstCTA={text("firstCTA", "Book a call")}
+      secondCTA={text("secondCTA", "Learn more")}></Hero>
   );
 };
