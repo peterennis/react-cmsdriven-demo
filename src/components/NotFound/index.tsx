c7393ed4cec4e2ae31a2be0fd3df6b14daa4b31b
peterennis <peterennis@yahoo.com>
2020-05-24 17:31:02 -0700

    wip(ts): convert NotFound to tsx and fix <p> format

--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-const NotFound = props => {
+const NotFound = (props: { description: any; }) => {
   return (
     <section className="fdb-block">
-      <p
+      <p>
         className="lead"
         dangerouslySetInnerHTML={{
           __html: props.description
         }}
-      />
+      </p>
     </section>
   );
 };
